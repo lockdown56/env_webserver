89be55f535b634732ec1eabdb7705097a112b555
lockdown56 <zhangzhiqiang56@163.com>
2018-02-19 17:51:12 +0800

    增加安装依赖功能

---
+++ scripts/dependence.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# 安装依赖
+
+# 检测并安装指定依赖
+checkAndInstall(){
+  pkg=$1
+  dpkg --get-selections | grep $pkg | grep "\<install\>"
+  if [ $? -ne 0 ]
+  then
+    sudo apt-get install -y $pkg
+  fi
+}
+
+# 定义依赖库数组, for ubuntu
+# libs_arr="gcc gcc-c++ automake libtool make cmake zlib1g zlib.dev"
+libs_arr=()
+libs_arr+=(build-essential)
+libs_arr+=(gcc gcc-c++ libtool automake make cmake autoconf)
+libs_arr+=(zlib1g zlib1g.dev openssl libssl-dev)
+libs_arr+=(libpcre3 libpcre3-dev)
+
+# 遍历安装依赖
+for lib in ${libs_arr[@]}
+do
+  checkAndInstall $lib
+done
+
+exit 0
---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+arr=()
+arr=(a b c)
+arr+=(d e)
+echo ${arr[@]}
+
+exit 0
