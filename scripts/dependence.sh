20e2c9d306eda861f6a9563a7dba0f8229bf5cc8
lockdown56 <zhangzhiqiang56@163.com>
2018-02-20 00:00:43 +0800

    检查目标机器是否安装 python, 如果没有安装就先安装

--- scripts/dependence.sh
+++ scripts/dependence.sh
@@ -14,13 +14,13 @@ checkAndInstall(){
 # 定义依赖库数组, for ubuntu
 # libs_arr="gcc gcc-c++ automake libtool make cmake zlib1g zlib.dev"
 libs_arr=()
+libs_arr+=(python)
 libs_arr+=(build-essential)
 libs_arr+=(gcc gcc-c++ libtool automake make cmake autoconf)
 libs_arr+=(zlib1g zlib1g.dev openssl libssl-dev)
 libs_arr+=(libpcre3 libpcre3-dev)
 libs_arr+=(sysv-rc-conf)
 
-sudo apt-get update
 # 遍历安装依赖
 for lib in ${libs_arr[@]}
 do
