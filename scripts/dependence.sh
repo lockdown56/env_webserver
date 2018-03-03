559b2fa639875ff06b017063d28c92ab3b6faf3d
lockdown56 <zhangzhiqiang56@163.com>
2018-03-03 09:00:43 +0800

    php 编译

--- scripts/dependence.sh
+++ scripts/dependence.sh
@@ -4,7 +4,7 @@
 # 检测并安装指定依赖
 checkAndInstall(){
   pkg=$1
-  dpkg --get-selections | grep $pkg | grep "\<install\>"
+  dpkg --get-selections | grep "\<$pkg\>" | grep "\<install\>"
   if [ $? -ne 0 ]
   then
     sudo apt-get install -y $pkg
@@ -19,6 +19,8 @@ libs_arr+=(gcc gcc-c++ libtool automake make cmake autoconf)
 libs_arr+=(zlib1g zlib1g.dev openssl libssl-dev)
 libs_arr+=(libpcre3 libpcre3-dev)
 libs_arr+=(sysv-rc-conf)
+libs_arr+=(libxml2 libxml2-dev php5-openssl libcurl4-openssl-dev pkg-config)
+libs_arr+=(libjpeg-dev libpng12-dev libfreetype6-dev libmcrypt-dev libmcrypt4)
 
 # 遍历安装依赖
 for lib in ${libs_arr[@]}
